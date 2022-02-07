e56d3b65024f554a0cbeeb077483d6232ee39d57
Loïc Mangeonjean <loic@codingame.com>
2022-02-07 18:30:52 +0100

    feat: Export hooks

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { monaco } from '@codingame/monaco-editor-wrapper'
+import { useThemeData, useUserConfiguration } from './hooks'
 import MonacoEditor, { MonacoEditorProps } from './MonacoEditor'
 
 export default MonacoEditor
 export {
-  monaco
+  monaco,
+  useThemeData,
+  useUserConfiguration
 }
 export type {
   MonacoEditorProps
